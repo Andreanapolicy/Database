b07b97156c42d9d8e488f9e5e78514d40fe758c0
Andrey Dudin <andrewdudin1337@gmail.com>
2022-02-25 01:17:06 +0300

    Добавил 5 задание для 3 лабы + подредактировал 4 задание(заполнение данных)

--- labs/lab3/task4.sql
+++ labs/lab3/task4.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE BookShop
-COLLATE Cyrillic_General_CI_AS;
-
+DROP DATABASE BookShop;
+CREATE DATABASE BookShop;
+# COLLATE Cyrillic_General_CI_AS;
+USE BookShop;
 CREATE TABLE IF NOT EXISTS author (
     id_author INT PRIMARY KEY AUTO_INCREMENT,
     birthday DATE NOT NULL,
@@ -44,9 +45,99 @@ CREATE TABLE IF NOT EXISTS lending_data (
     FOREIGN KEY (id_reader) REFERENCES reader(id_reader)
 );
 
-CREATE TABLE IF NOT EXISTS lending_data (
+CREATE TABLE IF NOT EXISTS lending_store (
     id_book INT NOT NULL,
     id_lending_data INT NOT NULL,
     FOREIGN KEY (id_book) REFERENCES book(id_book),
     FOREIGN KEY (id_lending_data) REFERENCES lending_data(id_lending_data)
 );
+
+# Authors
+
+INSERT INTO author(birthday, name, address, eye_color)
+VALUE ('2020-12-12', 'Александр', 'Успенская, д. 12', 'зеленый');
+
+INSERT INTO author(birthday, name, address, eye_color)
+VALUE ('1940-02-02', 'Владимир', 'Крайовская, д. 12', 'карий');
+
+INSERT INTO author(birthday, name, address, eye_color)
+VALUE ('2001-08-04', 'Петр', 'Уличная, д. 12', 'синий');
+
+INSERT INTO author(birthday, name, address, eye_color)
+VALUE ('1998-11-06', 'Николай', 'Владимирская, д. 12', 'голубосерый');
+
+# Books
+
+INSERT INTO book(id_author, creation_date, price, name)
+VALUE ('1', '2002-12-12', '1235', 'Зеленый чай и как правильно его готовить');
+
+INSERT INTO book(id_author, creation_date, price, name)
+VALUE ('1', '2012-12-12', '4300', 'Черный чай и как правильно его готовить');
+
+INSERT INTO book(id_author, creation_date, price, name)
+VALUE ('1', '2022-12-12', '9949', 'Белый чай и как правильно его готовить');
+
+INSERT INTO book(id_author, creation_date, price, name)
+VALUE ('2', '1989-12-12', '3', 'Как не готовить зеленый чай');
+
+# Readers
+
+INSERT INTO reader(birthday, name, address, eye_color)
+VALUE ('2020-12-12', 'Василий', 'Успенская, д. 12', 'зеленый');
+
+INSERT INTO reader(birthday, name, address, eye_color)
+VALUE ('1940-02-02', 'Илья', 'Крайовская, д. 12', 'карий');
+
+INSERT INTO reader(birthday, name, address, eye_color)
+VALUE ('2001-08-04', 'Иван', 'Уличная, д. 12', 'синий');
+
+INSERT INTO reader(birthday, name, address, eye_color)
+VALUE ('1998-11-06', 'Петр', 'Владимирская, д. 12', 'голубосерый');
+
+# Ducks
+
+INSERT INTO duck(id_reader, birthday, name, rating)
+VALUE ('1', '1940-02-02', 'Урчатель', '2');
+
+INSERT INTO duck(id_reader, birthday, name, rating)
+VALUE ('2', '2004-02-02', 'Какушек', '3');
+
+INSERT INTO duck(id_reader, birthday, name, rating)
+VALUE ('3', '1980-02-02', 'Стонатель', '4');
+
+INSERT INTO duck(id_reader, birthday, name, rating)
+VALUE ('4', '1937-02-02', 'Мамин соблазнитель', '1');
+
+INSERT INTO duck(id_reader, birthday, name, rating)
+VALUE ('1', '2001-02-02', 'Гиспофилиций', '3');
+
+# Lending data
+
+INSERT INTO lending_data(id_reader, start_date, end_date, pay)
+VALUE ('1', '2002-02-02', '2002-02-03', '300');
+
+INSERT INTO lending_data(id_reader, start_date, end_date, pay)
+VALUE ('2', '2002-02-02', '2002-02-03', '300');
+
+INSERT INTO lending_data(id_reader, start_date, end_date, pay)
+VALUE ('1', '2002-02-04', '2002-02-05', '300');
+
+INSERT INTO lending_data(id_reader, start_date, end_date, pay)
+VALUE ('3', '2012-02-02', '2013-02-03', '300');
+
+INSERT INTO lending_data(id_reader, start_date, end_date, pay)
+VALUE ('4', '2014-02-02', '2014-02-03', '300');
+
+# Lending store
+
+INSERT INTO lending_store(id_book, id_lending_data)
+VALUE ('1', '1');
+
+INSERT INTO lending_store(id_book, id_lending_data)
+VALUE ('2', '1');
+
+INSERT INTO lending_store(id_book, id_lending_data)
+VALUE ('3', '2');
+
+INSERT INTO lending_store(id_book, id_lending_data)
+VALUE ('4', '3');
---
+++ labs/lab3/task5.sql
@@ -0,0 +1,145 @@
+DROP DATABASE CarHotel;
+CREATE DATABASE CarHotel;
+# COLLATE Cyrillic_General_CI_AS;
+USE CarHotel;
+
+CREATE TABLE IF NOT EXISTS client (
+    id_client INT PRIMARY KEY AUTO_INCREMENT,
+    birthday DATE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    eye_color VARCHAR(50) NULL
+);
+
+CREATE TABLE IF NOT EXISTS cleaner (
+    id_cleaner INT PRIMARY KEY AUTO_INCREMENT,
+    birthday DATE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    eye_color VARCHAR(50) NULL
+);
+
+CREATE TABLE IF NOT EXISTS car (
+    id_car INT PRIMARY KEY AUTO_INCREMENT,
+    id_client INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    birthday DATE NOT NULL,
+    price INT NOT NULL,
+    FOREIGN KEY (id_client) REFERENCES client(id_client)
+);
+
+CREATE TABLE IF NOT EXISTS hotel_room (
+    id_hotel_room INT PRIMARY KEY AUTO_INCREMENT,
+    id_cleaner INT NOT NULL,
+    create_date DATE NOT NULL,
+    price INT NOT NULL,
+    style_name VARCHAR(100) NOT NULL,
+    number VARCHAR(50) NULL,
+    FOREIGN KEY (id_cleaner) REFERENCES cleaner(id_cleaner)
+);
+
+CREATE TABLE IF NOT EXISTS booking_data (
+    id_booking_data INT PRIMARY KEY AUTO_INCREMENT,
+    id_client INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    pay REAL NOT NULL,
+    FOREIGN KEY (id_client) REFERENCES client(id_client)
+);
+
+CREATE TABLE IF NOT EXISTS booking_store (
+    id_hotel_room INT NOT NULL,
+    id_booking_data INT NOT NULL,
+    FOREIGN KEY (id_hotel_room) REFERENCES hotel_room(id_hotel_room),
+    FOREIGN KEY (id_booking_data) REFERENCES booking_data(id_booking_data)
+);
+
+# client
+
+INSERT INTO client(birthday, name, address, eye_color)
+VALUE ('2020-12-12', 'Александр', 'Успенская, д. 12', 'зеленый');
+
+INSERT INTO client(birthday, name, address, eye_color)
+VALUE ('1940-02-02', 'Владимир', 'Крайовская, д. 12', 'карий');
+
+INSERT INTO client(birthday, name, address, eye_color)
+VALUE ('2001-08-04', 'Петр', 'Уличная, д. 12', 'синий');
+
+INSERT INTO client(birthday, name, address, eye_color)
+VALUE ('1998-11-06', 'Николай', 'Владимирская, д. 12', 'голубосерый');
+
+# hotel_room
+
+INSERT INTO hotel_room(id_cleaner, create_date, price, style_name, number)
+VALUE ('1', '2002-12-12', '1235', 'Виницианский стиль', '203');
+
+INSERT INTO hotel_room(id_cleaner, create_date, price, style_name, number)
+VALUE ('3', '2012-12-12', '4300', 'Андреанский стиль', '506');
+
+INSERT INTO hotel_room(id_cleaner, create_date, price, style_name, number)
+VALUE ('1', '2022-12-12', '9949', 'Белый чай и как правильно его готовить', '777');
+
+INSERT INTO hotel_room(id_cleaner, create_date, price, style_name, number)
+VALUE ('2', '1989-12-12', '3', 'Как не готовить зеленый чай', '103');
+
+# cleaner
+
+INSERT INTO cleaner(birthday, name, address, eye_color)
+VALUE ('2020-12-12', 'Василий', 'Успенская, д. 12', 'зеленый');
+
+INSERT INTO cleaner(birthday, name, address, eye_color)
+VALUE ('1940-02-02', 'Илья', 'Крайовская, д. 12', 'карий');
+
+INSERT INTO cleaner(birthday, name, address, eye_color)
+VALUE ('2001-08-04', 'Иван', 'Уличная, д. 12', 'синий');
+
+INSERT INTO cleaner(birthday, name, address, eye_color)
+VALUE ('1998-11-06', 'Петр', 'Владимирская, д. 12', 'голубосерый');
+
+# car
+
+INSERT INTO car(id_client, birthday, name, price)
+VALUE ('1', '1940-02-02', 'Урчатель', '2');
+
+INSERT INTO car(id_client, birthday, name, price)
+VALUE ('2', '2004-02-02', 'Какушек', '3');
+
+INSERT INTO car(id_client, birthday, name, price)
+VALUE ('3', '1980-02-02', 'Стонатель', '4');
+
+INSERT INTO car(id_client, birthday, name, price)
+VALUE ('4', '1937-02-02', 'Мамин соблазнитель', '1');
+
+INSERT INTO car(id_client, birthday, name, price)
+VALUE ('1', '2001-02-02', 'Гиспофилиций', '3');
+
+# booking_data
+
+INSERT INTO booking_data(id_client, start_date, end_date, pay)
+VALUE ('1', '2002-02-02', '2002-02-03', '300');
+
+INSERT INTO booking_data(id_client, start_date, end_date, pay)
+VALUE ('2', '2002-02-02', '2002-02-03', '300');
+
+INSERT INTO booking_data(id_client, start_date, end_date, pay)
+VALUE ('1', '2002-02-04', '2002-02-05', '300');
+
+INSERT INTO booking_data(id_client, start_date, end_date, pay)
+VALUE ('3', '2012-02-02', '2013-02-03', '300');
+
+INSERT INTO booking_data(id_client, start_date, end_date, pay)
+VALUE ('4', '2014-02-02', '2014-02-03', '300');
+
+# booking_store
+
+INSERT INTO booking_store(id_hotel_room, id_booking_data)
+VALUE ('1', '1');
+
+INSERT INTO booking_store(id_hotel_room, id_booking_data)
+VALUE ('2', '1');
+
+INSERT INTO booking_store(id_hotel_room, id_booking_data)
+VALUE ('3', '2');
+
+INSERT INTO booking_store(id_hotel_room, id_booking_data)
+VALUE ('4', '3');
