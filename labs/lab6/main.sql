11df3d498b9abcff4fad42507c45e1459c690c2f
Andrey Dudin <andrewdudin1337@gmail.com>
2022-03-26 13:47:18 +0300

    6 лаба. Выполнил 2 задание. Оттестировал

--- labs/lab6/main.sql
+++ labs/lab6/main.sql
@@ -14,3 +14,24 @@ ALTER TABLE `order` ADD FOREIGN KEY (id_dealer) REFERENCES dealer(id_dealer);
 ALTER TABLE `order` ADD FOREIGN KEY (id_pharmacy) REFERENCES pharmacy(id_pharmacy);
 
 ALTER TABLE `order` ADD FOREIGN KEY (id_production) REFERENCES production(id_production);
+
+# === 2. Выдать информацию по всем заказам лекарства “Кордерон” компании “Аргус” с указанием названий аптек, дат, объема заказов. ===
+
+SELECT pharmacy.name, `order`.date, `order`.quantity from medicine
+    INNER JOIN production ON medicine.id_medicine = production.id_medicine
+    INNER JOIN `order` ON production.id_production = `order`.id_production
+    INNER JOIN company ON production.id_company = company.id_company
+    INNER JOIN pharmacy ON `order`.id_pharmacy = pharmacy.id_pharmacy
+WHERE
+    medicine.name = 'Кордерон' AND
+    company.name = 'Аргус';
+
+#== Это для просмотра в контейнере ==
+# SELECT pharmacy.name, `order`.date, `order`.quantity from medicine
+#     INNER JOIN production ON medicine.id_medicine = production.id_medicine
+#     INNER JOIN `order` ON production.id_production = `order`.id_production
+#     INNER JOIN company ON production.id_company = company.id_company
+#     INNER JOIN pharmacy ON `order`.id_pharmacy = pharmacy.id_pharmacy
+# WHERE
+#     medicine.id_medicine = 10 AND
+#     company.id_company = 7;
