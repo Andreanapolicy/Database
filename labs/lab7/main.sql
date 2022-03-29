f24d1a6c185d26561dabbaebcd8268e5d2f3ef22
Andrey Dudin <andrewdudin1337@gmail.com>
2022-03-29 15:36:01 +0300

    7 лаба. Выполнил 2 задание

--- labs/lab7/main.sql
+++ labs/lab7/main.sql
@@ -14,3 +14,22 @@ ALTER TABLE lesson ADD FOREIGN KEY (id_group) REFERENCES `group`(id_group);
 ALTER TABLE mark ADD FOREIGN KEY (id_lesson) REFERENCES lesson(id_lesson);
 
 ALTER TABLE mark ADD FOREIGN KEY (id_student) REFERENCES student(id_student);
+
+# === 2. Выдать оценки студентов по информатике если они обучаются данному предмету. Оформить выдачу данных с использованием view ===
+
+CREATE VIEW informatics_marks AS
+    SELECT student.name, mark.mark from mark
+        LEFT JOIN lesson ON mark.id_lesson = lesson.id_lesson
+        LEFT JOIN student ON mark.id_student = student.id_student
+        LEFT JOIN subject ON lesson.id_subject = subject.id_subject
+    WHERE
+        subject.name = 'Информатика';
+
+#== Это для просмотра в контейнере ==
+# CREATE VIEW informatics_marks AS
+#     SELECT student.name, mark.mark from mark
+#         LEFT JOIN lesson ON mark.id_lesson = lesson.id_lesson
+#         LEFT JOIN student ON mark.id_student = student.id_student
+#         LEFT JOIN subject ON lesson.id_subject = subject.id_subject
+#     WHERE
+#         subject.id_subject = 2;
