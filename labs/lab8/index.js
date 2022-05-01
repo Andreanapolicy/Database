a9da7d1c9cb69b6f1d0d8d432667c63a213acc73
Andreanapolicy <daa.and@mail.ru>
2022-05-01 15:32:41 +0300

    lab8. Вставка одной записи

--- labs/lab8/index.js
+++ labs/lab8/index.js
@@ -20,8 +20,36 @@ async function main()
     //=== 3.2 Вставка записей ===
 
     //== Вставка 1 записи ==
-    collection.insertOne({
-
+    await collection.insertOne({
+        'room': {
+            'create_date': '2022-04-05',
+            'price': 500,
+            'style_name': 'Андреанский стиль',
+            'number': 301,
+            'cleaner': {
+                'birthday': '1987-12-10',
+                'name': 'Ашот',
+                'address': 'Ленового 8',
+                'eye_color': 'черный',
+            },
+        },
+        'client': {
+            'birthday': '1987-04-05',
+            'name': 'Антон',
+            'address': 'Урчалово 8',
+            'eye_color': 'зеленый',
+            'car': [
+                {
+                    'creation_date': '2022-04-05',
+                    'name': 'Урчатель',
+                    'price': 20000,
+                    'rating': 1
+                }
+            ]
+        },
+        'start_date': '2023-04-05',
+        'end_date': '2023-05-05',
+        'pay': 6504,
     });
 
     return 'done.';
